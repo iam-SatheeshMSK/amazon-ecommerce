8f7c0d6bb0cd71ba368f636c0975b4d924061ec7
satheesh <skm.devops33@gmail.com>
2025-05-21 21:45:32 -0500

    Initial commit: Spring Boot web app setup

---
+++ src/main/java/com/amazon/ecommerce/AmazonWebappApplication.java
@@ -0,0 +1,11 @@
+package com.amazon.ecommerce; // Fix package name
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonWebappApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AmazonWebappApplication.class, args);
+    }
+}
---
+++ src/test/java/com/amazon/ecommerce/amazon_webapp/AmazonWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazon.ecommerce.amazon_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
